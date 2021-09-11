91f4df196f7c32faf59638197065b83ceb4b61e0
PacoV3 <franj.vents@gmail.com>
2021-09-11 10:39:40 -0500

    Comments for all the functions

--- PROYECTO-01-VENTURA-FRANCISCO.py
+++ PROYECTO-01-VENTURA-FRANCISCO.py
@@ -35,7 +35,10 @@ def login(users):
 
 
 def products_dict(container):
+    '''Function for finding the ammount of times a product was sold'''
     products = {}
+    # For all elements inside the container save in the dict a counter in
+    # the product key
     for element in container:
         product_id = element[1]
         if product_id in products:
@@ -46,13 +49,16 @@ def products_dict(container):
 
 
 def organize_products(group):
+    '''Function for organizing the products from most sold to least sold'''
     group_dict = products_dict(group)
     group_items_list = list(group_dict.items())
+    # Order by the second element in the tuple
     result = sorted(group_items_list, key=lambda x: x[1], reverse=True)
     return result
 
 
 def print_point11(list, lifestore_products, attribute):
+    '''Function for printing the format for the point 1.1'''
     for product_id, value in list:
         product_name = lifestore_products[product_id - 1][1].split(',')[0]
         print(f'"{product_name}" tuvo {value} {attribute}')
@@ -60,12 +66,14 @@ def print_point11(list, lifestore_products, attribute):
 
 
 def print_point12(list, attribute):
+    '''Function for printing the format for the point 1.2'''
     for category, value in list:
         print(f'La categoría "{category}" tuvo {value} {attribute}')
     print()
 
 
 def print_point2(list, lifestore_products, attribute):
+    '''Function for printing the format for the point 2'''
     for product_id, value in list:
         product_name = lifestore_products[product_id - 1][1].split(',')[0]
         print(f'"{product_name}" tuvo {value:0.2f} {attribute}')
@@ -73,7 +81,10 @@ def print_point2(list, lifestore_products, attribute):
 
 
 def categories_dict(container, lifestore_products):
+    '''Function for grouping the categories and assigning the amount of 
+    times a product of the categorie was sold'''
     categories = {}
+    # For each element in the container get the category and save the counter
     for element in container:
         product_id = element[1]
         category = lifestore_products[product_id-1][3]
@@ -85,14 +96,20 @@ def categories_dict(container, lifestore_products):
 
 
 def organize_categories(group, lifestore_products):
+    '''Function to organize the categories from most sold to least'''
     group_dict = categories_dict(group, lifestore_products)
     group_items_list = list(group_dict.items())
+    # Order by the second element in the tuple
     result = sorted(group_items_list, key=lambda x: x[1])
     return result
 
 
 def product_reviews_dict(sales):
+    '''Function for grouping the products by their review and also the
+    amount of times a review was made'''
     reviews = {}
+    # For each sale get the review and save it together with a counter
+    # to calculate the average review for each product
     for sale in sales:
         refund = sale[4]
         if not refund:
@@ -107,22 +124,30 @@ def product_reviews_dict(sales):
 
 
 def organize_product_reviews(lifestore_sales):
+    '''Function to calculate the average review from all sales'''
     reviews = product_reviews_dict(lifestore_sales)
     reviews_list = list(reviews.items())
+    # Calculate the average for all products sold
     result = [(index, vals[0]/vals[1]) for index, vals in reviews_list]
+    # Order by average reviews 
     result = sorted(result, key=lambda x: x[1])
     return result
 
 
 def revenue_by_month_n_year(lifestore_sales, lifestore_products):
+    '''Function to group all sales by month and year'''
     years_n_months = {}
+    # For all sale inside lifestore_sales
     for _, product_id, _, sale_date, refund in lifestore_sales:
         product_price = lifestore_products[product_id-1][2]
         formatted_date = datetime.strptime(sale_date, '%d/%m/%Y')
         year = formatted_date.year
         month = formatted_date.month
+        # All products that were not refunded
         if not refund:
+            # Logic for grouping by year
             if year in years_n_months:
+                # Logic for grouping by month
                 if month in years_n_months[year]:
                     years_n_months[year][month] += product_price
                 else:
@@ -133,14 +158,20 @@ def revenue_by_month_n_year(lifestore_sales, lifestore_products):
 
 
 def calc_total_revenue(revenue_dict):
+    '''Function to calculate the total revenue'''
     total_revenue = 0
-    for months in revenue_dict.values():
-        total_revenue += sum(months.values())
+    # For all years in the revenue dict
+    for year in revenue_dict.values():
+        # Sum the revenue of all months (year.values()) and return the total
+        total_revenue += sum(year.values())
     return total_revenue
 
 
 def calc_revenue_by_year(revenue_dict):
+    '''Function to calculate the total revenue by all years'''
     year_revenue = {}
+    # For all years and months sum the total by month and store it
+    # by each year
     for year, months in revenue_dict.items():
         if year in year_revenue:
             year_revenue[year] += sum(months.values())
@@ -150,30 +181,37 @@ def calc_revenue_by_year(revenue_dict):
 
 
 def print_total_revenue(total_revenue):
+    '''Function to print the total revenue of the company'''
     print(f'Ingresos totales: {locale.currency(total_revenue, grouping=True)} MXN')
     print()
 
 
 def print_revenue_by_year(year_dict):
+    '''Function to print the total revenue by all years'''
     year_total = list(year_dict.items())
     year_total = sorted(year_total, key=lambda x: x[0])
     for year, total in year_total:
-        print(f'Ingresos en el año {year}: {locale.currency(total, grouping=True)} MXN')
+        print(
+            f'Ingresos en el año {year}: {locale.currency(total, grouping=True)} MXN')
     print()
 
 
 def print_revenue_by_month(revenue_dict):
+    '''Function to print the total revenue by all months and the best month of the last year'''
+    # Find the last year
     last_year = max(revenue_dict.keys())
     months_of_last_year = list(revenue_dict[last_year].items())
+    # Sort the months
     months_of_last_year = sorted(months_of_last_year, key=lambda x: x[0])
     max_month = max(months_of_last_year, key=lambda x: x[1])
     for month, total in months_of_last_year:
         month_name = datetime.strptime(str(month), "%m").strftime('%B')
-        print(f'Ingresos en el mes de {month_name} del año {last_year}: {locale.currency(total, grouping=True)} MXN')
+        print(
+            f'Ingresos en el mes de {month_name} del año {last_year}: {locale.currency(total, grouping=True)} MXN')
     print()
     month_name = datetime.strptime(str(max_month[0]), "%m").strftime('%B')
-    print(f'El mes de {month_name} del año {last_year} tuvo la mayor cantidad ingresos con: {locale.currency(max_month[1], grouping=True)} MXN')
-
+    print(
+        f'El mes de {month_name} del año {last_year} tuvo la mayor cantidad ingresos con: {locale.currency(max_month[1], grouping=True)} MXN')
 
 
 def main():
@@ -185,8 +223,9 @@ def main():
              {'username': 'javier123', 'password': 'javier123'},
              {'username': 'javier456', 'password': 'javier456'}]
     # Go through the login process
-    # login(users)
-    # print()
+    login(users)
+    print()
+    # Pass through all the different types of analysis
     print('------------------- BEST SELLERS -------------------------------------------------------')
     sellers = organize_products(lifestore_sales)
     print_point11(sellers[0:50], lifestore_products, 'ventas.')
@@ -197,7 +236,8 @@ def main():
     category_sellers = organize_categories(lifestore_sales, lifestore_products)
     print_point12(category_sellers, 'ventas.')
     print('------------- LEAST SEARCHED BY CATEGORY -----------------------------------------------')
-    category_least_search = organize_categories(lifestore_searches, lifestore_products)
+    category_least_search = organize_categories(
+        lifestore_searches, lifestore_products)
     print_point12(category_least_search, 'búsquedas.')
     print('--------------- BEST REVIEWS BY PRODUCT ------------------------------------------------')
     reviews = organize_product_reviews(lifestore_sales)
@@ -205,7 +245,8 @@ def main():
     print('--------------- WORST REVIEWS BY PRODUCT -----------------------------------------------')
     print_point2(reviews[:20], lifestore_products, 'en calificación.')
     print('------------------------- REVENUE ------------------------------------------------------')
-    year_n_month_sells = revenue_by_month_n_year(lifestore_sales, lifestore_products)
+    year_n_month_sells = revenue_by_month_n_year(
+        lifestore_sales, lifestore_products)
     total_revenue = calc_total_revenue(year_n_month_sells)
     print_total_revenue(total_revenue)
     year_revenue = calc_revenue_by_year(year_n_month_sells)
